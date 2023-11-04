15eb838bd20056736e320142e7a6fe479846906b
Becky449 <rabnab12@gmail.com>
2023-11-04 18:33:44 +0300

    edit cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,41 +1,24 @@
-# frozen_string_literal: true
 
 class Ability
-  include CanCan::Ability
+  include CanCanCan::Ability
 
   def initialize(user)
-        user ||= User.new # Guest user
-    
-        # Define abilities here
-        can :read, Agrovet, user_id: user.id
+    user ||= User.new # Guest user
 
-        # Add other abilities as needed
-  
-    
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-      return unless user.admin?
-       can :manage, :all
-      end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    # Define abilities for a guest user here
+    can :read, Agrovet, user_id: user.id
+
+    # Define abilities for the user here
+    return unless user.present?
+
+    # If the user is an admin, grant them full management abilities
+    if user.admin?
+      can :manage, :all
+    end
+
+    # Define other abilities as needed for non-admin users
+
+    # Example: Allow users to read articles only if they are published
+    # can :read, Article, published: true
   end
 end
