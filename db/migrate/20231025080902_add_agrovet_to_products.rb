5a7aa0bd5799418d48a7710eee385d174bc83717
Becky449 <rabnab12@gmail.com>
2023-11-12 20:02:05 +0300

    fix db

--- db/migrate/20231025080902_add_agrovet_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddAgrovetToProducts < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :products, :agrovet, foreign_key: true
-  end
-end
--- db/migrate/20231106045836_remove_agrovet_id_from_products.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAgrovetIdFromProducts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :products, :agrovet_id
-  end
-end
