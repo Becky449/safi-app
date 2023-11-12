ce8dcaa7d6f3325a912ac29ab5855970689bb60a
Becky449 <rabnab12@gmail.com>
2023-11-12 19:31:53 +0300

    Update Migrations db

--- db/migrate/20231106043125_add_product_quantities_to_data_entries.rb
+++
@@ -1,7 +0,0 @@
-class AddProductQuantitiesToDataEntries < ActiveRecord::Migration[7.0]
-  def change
-    add_column :data_entries, :product_5_quantity, :integer
-    add_column :data_entries, :product_6_quantity, :integer
-    add_column :data_entries, :product_7_quantity, :integer
-  end
-end
--- db/migrate/20231106044146_add_product_quantities_to_orders.rb
+++
@@ -1,7 +0,0 @@
-class AddProductQuantitiesToOrders < ActiveRecord::Migration[7.0]
-  def change
-    add_column :orders, :product_5_quantity, :integer
-    add_column :orders, :product_6_quantity, :integer
-    add_column :orders, :product_7_quantity, :integer
-  end
-end
