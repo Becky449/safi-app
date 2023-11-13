5b9697967faab314cb2a016a5a3ab5aea7148b46
Becky449 <rabnab12@gmail.com>
2023-11-13 08:08:32 +0300

    Checkout Working version

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,4 @@ class Product < ApplicationRecord
     has_many :data_entries
     has_many :agrovets, through: :data_entries
     has_many :orders
-    belongs_to :user
-    belongs_to :agrovet
 end
--- db/migrate/20231025080902_add_agrovet_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddAgrovetToProducts < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :products, :agrovet, foreign_key: true
-  end
-end
