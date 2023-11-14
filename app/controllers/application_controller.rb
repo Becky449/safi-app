eb0ab3b2d3c1c2088ccb3361f39521118cd92ba1
Becky449 <rabnab12@gmail.com>
2023-11-14 12:13:55 +0300

    Remove roles input field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < ActionController::Base
         # if current_user.admin?
         if resource.admin?
             pages_admin_path
+            
         elsif resource.manager?
             pages_manager_path
         else
