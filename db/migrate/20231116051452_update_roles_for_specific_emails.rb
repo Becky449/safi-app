aafc1d7a040c4d9072fae259f484a0a9a836f76f
Becky449 <rabnab12@gmail.com>
2023-11-16 08:22:26 +0300

    Update roles to Integers

--- db/migrate/20231116051452_update_roles_for_specific_emails.rb
+++ db/migrate/20231116051452_update_roles_for_specific_emails.rb
@@ -1,6 +1,6 @@
 class UpdateRolesForSpecificEmails < ActiveRecord::Migration[7.0]
   def change
-    User.find_by(email: 'safiorganics1@gmailcom')&.update(role: :admin)
-    User.find_by(email: 'joycekamande54@gmail.com')&.update(role: :manager)
+    User.find_by(email: 'safiorganics1@gmailcom')&.update(role: 1)
+    User.find_by(email: 'joycekamande54@gmail.com')&.update(role: 2)
   end
 end
